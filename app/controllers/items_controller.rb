b671eb9248ad95966ed95b5daada2ea0847dc113
Masaki Hasegawa <masaki.hasegawa@gmail.com>
2015-11-22 12:12:12 +0900

    index開発

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 class ItemsController < ApplicationController
+
   def show
-  #Itemというモデルに対してfindメソッドでidを検索させている
-  @item = Item.find(params[:id])
+    #Itemというモデルに対してfindメソッドでidを検索させている
+    @item = Item.find(params[:id])
+  end
+
+  def index
+    @items = Item.all
   end
 end
